08f80f95763711be119c8ca0bae91c8f9e982e00
erkrystof <15527821+erkrystof@users.noreply.github.com>
2025-10-17 21:56:31 -0500

    RUNS_ON_GLOBAL_CACHE_PREFIX support for re-using cache across different repos.

--- dist/save/index.js
+++ dist/save/index.js
@@ -73357,6 +73357,11 @@ function getCacheVersion(paths, compressionMethod, enableCrossOsArchive = false)
         .digest("hex");
 }
 function getS3Prefix(paths, { compressionMethod, enableCrossOsArchive }) {
+    const globalCachePrefix = process.env.RUNS_ON_GLOBAL_CACHE_PREFIX;
+    if (globalCachePrefix) {
+        const version = getCacheVersion(paths, compressionMethod, enableCrossOsArchive);
+        return [globalCachePrefix, version].join("/");
+    }
     const repository = process.env.GITHUB_REPOSITORY;
     const version = getCacheVersion(paths, compressionMethod, enableCrossOsArchive);
     return ["cache", repository, version].join("/");
